d501d7127b53f20ebedb7e4aba9512d465910c69
Vien <vienv2@illinois.edu>
2021-08-02 16:04:53 -0500

    Formatting

--- server/dist/config/db.config.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports["default"] = void 0;
-var url = 'mongodb+srv://ludi_user:ludi_password@ludicluster.rtcvv.mongodb.net/ludi_db?retryWrites=true&w=majority';
-var _default = url;
-exports["default"] = _default;
