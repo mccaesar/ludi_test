839f263b03f2cf46995cfe869ad3b693d023a2bc
Vien Vuong <vienv2@illinois.edu>
2021-10-13 09:20:32 -0500

    Changes tab title and favicon.

--- client/src/pages/UploadPage/index.jsx
+++ client/src/pages/UploadPage/index.jsx
@@ -7,27 +7,29 @@ export const UploadPage = () => {
   return (
     <WithFooter>
       <Navbar />
-
-      <Center py={10}>
-        <Text fontSize="lg">
-          {' '}
-          We welcome feedback to be emailed to us at ludi@illinois.edu <br />
-          Additional resources can be submitted through the form below{' '}
+      <>
+        {/* <Center py={10}> */}
+        <Text fontSize="lg" textAlign="center">
+          We welcome feedback and suggestions for content to be included on this
+          site.
+          Please email us at ludi@illinois.edu.
+          Additional resources can be submitted through the form below.
         </Text>
-      </Center>
-      <Center py={2}>
-        <iframe
-          title="Upload Form"
-          src="https://docs.google.com/forms/d/e/1FAIpQLScDnl0EqT7Kb_tsGRcqGHFEVZjN8DqKSpwql7CUwsmw_Qs9Iw/viewform?embedded=true"
-          width="700"
-          height="700"
-          frameborder="0"
-          marginheight="0"
-          marginwidth="0"
-        >
-          Loading…
-        </iframe>
-      </Center>
+        {/* </Center> */}
+        <Center py={2}>
+          <iframe
+            title="Upload Form"
+            src="https://docs.google.com/forms/d/e/1FAIpQLScDnl0EqT7Kb_tsGRcqGHFEVZjN8DqKSpwql7CUwsmw_Qs9Iw/viewform?embedded=true"
+            width="700"
+            height="700"
+            frameborder="0"
+            marginheight="0"
+            marginwidth="0"
+          >
+            Loading…
+          </iframe>
+        </Center>
+      </>
     </WithFooter>
   );
 };
